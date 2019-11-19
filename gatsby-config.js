381553714d94a81dbd9bfd11c40c01ac9b2518e7
Will Hawks <will@creativedistillery.com>
2019-11-19 15:58:08 -0600

    remove page transition plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-transition-link`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
