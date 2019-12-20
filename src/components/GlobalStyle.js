cdd1970914eb96fb1ca5e11bfad1d834ecf36c7b
Will Hawks <willkhawks@gmail.com>
2019-12-20 13:50:27 -0600

    auto hide scrollbars in edge

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -5,6 +5,7 @@ const GlobalStyle = createGlobalStyle`
   html {
     box-sizing: border-box;
     font-size: 10px;
+    -ms-overflow-style: -ms-autohiding-scrollbar;
   }
 
   *, *:before, *:after {
